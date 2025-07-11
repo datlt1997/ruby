f6ff97c1126e102cce2a04e72e66568cd97dfa66
datlt <datlt.nta@gmail.com>
2025-07-11 09:23:38 +0700

    [update] update crlf to lf

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_06_28_155419) do
-  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
+  create_table "posts", charset: "utf8", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
