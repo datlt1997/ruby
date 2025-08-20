97bdaa82e419e401b7d828a2eb57645a527e5ab6
NhutD_NM <nhutnm.nta@gmail.com>
2025-08-20 11:05:02 +0700

    [update] update multi language user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2025_08_12_075333) do
-  create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
+  create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
     t.bigint "record_id", null: false
@@ -21,7 +21,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_12_075333) do
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
   end
 
-  create_table "active_storage_blobs", charset: "utf8", force: :cascade do |t|
+  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "key", null: false
     t.string "filename", null: false
     t.string "content_type"
@@ -33,13 +33,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_12_075333) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "active_storage_variant_records", charset: "utf8", force: :cascade do |t|
+  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
-  create_table "post_tags", charset: "utf8", force: :cascade do |t|
+  create_table "post_tags", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "post_id", null: false
     t.bigint "tag_id", null: false
     t.datetime "created_at", null: false
@@ -48,7 +48,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_12_075333) do
     t.index ["tag_id"], name: "index_post_tags_on_tag_id"
   end
 
-  create_table "posts", charset: "utf8", force: :cascade do |t|
+  create_table "posts", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
@@ -58,7 +58,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_12_075333) do
     t.text "short_description"
   end
 
-  create_table "prizes", charset: "utf8", force: :cascade do |t|
+  create_table "prizes", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name", null: false
     t.integer "quantity", null: false
     t.text "description"
@@ -66,7 +66,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_12_075333) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "select_numbers", charset: "utf8", force: :cascade do |t|
+  create_table "select_numbers", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.integer "number"
     t.datetime "created_at", null: false
@@ -74,13 +74,13 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_12_075333) do
     t.index ["user_id"], name: "index_select_numbers_on_user_id"
   end
 
-  create_table "tags", charset: "utf8", force: :cascade do |t|
+  create_table "tags", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", charset: "utf8", force: :cascade do |t|
+  create_table "users", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.string "email"
     t.string "password_digest"
     t.integer "role"
@@ -90,7 +90,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_12_075333) do
     t.text "phone"
   end
 
-  create_table "winners", charset: "utf8", force: :cascade do |t|
+  create_table "winners", charset: "utf8mb4", collation: "utf8mb4_unicode_ci", force: :cascade do |t|
     t.bigint "select_number_id", null: false
     t.bigint "prize_id", null: false
     t.datetime "created_at", null: false
